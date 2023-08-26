8ef70602491d401066a7f2a0e7bb3fcf7360a61e
PeaTearG <63596970+PeaTearG@users.noreply.github.com>
2023-08-26 00:31:59 -0400

    Update sdpctl.rb

--- sdpctl.rb
+++ sdpctl.rb
@@ -3,21 +3,13 @@ class Sdpctl < Formula
   homepage "https://github.com/appgate/sdpctl"
   version "2023.08.18"
 
-  if OS.mac?
+  on_macos do
     if RUBY_PLATFORM.include?("x86_64")
       url "https://github.com/appgate/sdpctl/releases/download/#{version}/sdpctl_#{version}_darwin_amd64.tar.gz"
       sha256 "b8b8ee4b601b0b6ffea60160824e6f187caf842453fdc7b661adf4f056c0eacc"
     else
       url "https://github.com/appgate/sdpctl/releases/download/#{version}/sdpctl_#{version}_darwin_arm64.tar.gz"
       sha256 "515a17310f629eebcf6935b1b4ddf5e6e7d052152e22704b1c568a8132ad1bca"
-    end
-  else
-    if RUBY_PLATFORM.include?("x86_64")
-      url "https://github.com/appgate/sdpctl/releases/download/#{version}/sdpctl_#{version}_linux_amd64.tar.gz"
-      sha256 "abc4f5c96632de5fb63758893086cb1402dc66db1378bf51878dd292678620d2"
-    else
-      url "https://github.com/appgate/sdpctl/releases/download/#{version}/sdpctl_#{version}_linux_arm64.tar.gz"
-      sha256 "114730ecfe16cc12a681033e831d25aee866a21b042078c16b3c27bf05aa0966"
     end
   end
 
