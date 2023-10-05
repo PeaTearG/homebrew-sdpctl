3683d53b6bb0d98d37544009715712fd78e66c2b
PeaTearG <63596970+PeaTearG@users.noreply.github.com>
2023-10-05 10:18:19 -0400

    Update sdpctl.rb to 2023.10.04

--- sdpctl.rb
+++ sdpctl.rb
@@ -1,15 +1,15 @@
 class Sdpctl < Formula
   desc "Appgate sdpctl is compatible with SDP Collectives versions 6 and newer"
   homepage "https://github.com/appgate/sdpctl"
-  version "2023.09.14"
+  version "2023.10.04"
 
   on_macos do
     if RUBY_PLATFORM.include?("x86_64")
       url "https://github.com/appgate/sdpctl/releases/download/#{version}/sdpctl_#{version}_darwin_amd64.tar.gz"
-      sha256 "e6ea2b70848857f619c13d3c33242db9c949fe29614cc75510664ead83383736"
+      sha256 "2fc3a6e25389339f858c4e382d3fbab0213b785a3d3cb89e837877b61320c486"
     else
       url "https://github.com/appgate/sdpctl/releases/download/#{version}/sdpctl_#{version}_darwin_arm64.tar.gz"
-      sha256 "6d86197557d5207dd082524f1c56be20830c9398d49b260158f96be02e68de8e"
+      sha256 "049b6cdb8eb1a86f8ef8141f72549cb445c66fe7dce304b3af6568187ce37781"
     end
   end
 
